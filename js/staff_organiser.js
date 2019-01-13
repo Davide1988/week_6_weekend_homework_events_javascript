d09b79d8d80390f37f00bac39a18289b07cffc1d
Davide Venditti <davide_335@msn.com>
2019-01-13 20:29:24 +0000

    add ul for new staff element saved into ul of section

--- js/staff_organiser.js
+++ js/staff_organiser.js
@@ -12,18 +12,22 @@ const makeAnHTMLLi = function(text) {
   const li = document.createElement('li');
   li.textContent = text;
   return li;
+
 }
 
+  const reverseDate = function (date) {
+      return date.split("-").reverse().join("-")
+  }
+
+
   handleNewStaffInput = function(event) {
-
-
     event.preventDefault();
 
     const name = makeAnHTMLLi(`Name: ${event.target.name.value.charAt(0).toUpperCase() + event.target.name.value.slice(1)}`)
 
     const surname = makeAnHTMLLi(`Surname: ${event.target.surname.value.charAt(0).toUpperCase() + event.target.surname.value.slice(1)}`)
 
-    const startingDate = makeAnHTMLLi(`Starting date: ${event.target.startingDate.value}`)
+    const startingDate = makeAnHTMLLi(`Starting date: ${reverseDate(event.target.startingDate.value)}`)
 
     const title = makeAnHTMLLi(`Title: ${event.target.title.value}`)
 
@@ -37,31 +41,48 @@ const makeAnHTMLLi = function(text) {
     const displayFrontOfHouse = document.querySelector("#foh")
 
 
+
+
     const detailsNewStaff = [name, surname, startingDate, title, salary]
 
 
 
 
       if (detailsNewStaff[3].textContent == "Title: Manager") {
+        const ulForManager = document.createElement('ul')
+        ulForManager.className = "manager"
+        displayManager.appendChild(ulForManager)
         detailsNewStaff.forEach((el) => {
-          return displayManager.appendChild(el)
+          return ulForManager.appendChild(el)
       })
     }
     else if (detailsNewStaff[3].textContent == "Title: Chef") {
+      const ulForChef = document.createElement('ul')
+      ulForChef.className = "chef"
+      displayChef.appendChild(ulForChef)
       detailsNewStaff.forEach((el) => {
-        return displayChef.appendChild(el)
+        return ulForChef.appendChild(el)
       })
     }
-    else  detailsNewStaff.forEach((el) => {
-        return displayFrontOfHouse.appendChild(el)
+
+    else if (detailsNewStaff[3].textContent == "Title: FOH"){
+
+        const ulForStaff = document.createElement('ul')
+        ulForStaff.className = "staff"
+
+        displayFrontOfHouse.appendChild(ulForStaff)
+        detailsNewStaff.forEach((el) => {
+        return ulForStaff.appendChild(el)
       })
-
-
+    }
 
 
     event.target.reset()
 
   }
+
+
+
 
   handleDeleteAll = function() {
     const displayManager = document.querySelector("#manager")
