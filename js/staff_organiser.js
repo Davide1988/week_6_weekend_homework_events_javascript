4037942ca112fd2717034fd7b7d63fcda92a34c1
Davide Venditti <davide_335@msn.com>
2019-01-13 17:26:52 +0000

    add the delete button for the 3 classes : manager,chef,foh

--- js/staff_organiser.js
+++ js/staff_organiser.js
@@ -16,31 +16,59 @@ const makeAnHTMLLi = function(text) {
 
   handleNewStaffInput = function(event) {
 
+
     event.preventDefault();
 
-    const name = makeAnHTMLLi(`Name: ${event.target.name.value}`)
+    const name = makeAnHTMLLi(`Name: ${event.target.name.value.charAt(0).toUpperCase() + event.target.name.value.slice(1)}`)
 
-    const surname = makeAnHTMLLi(`Surname: ${event.target.surname.value}`)
+    const surname = makeAnHTMLLi(`Surname: ${event.target.surname.value.charAt(0).toUpperCase() + event.target.surname.value.slice(1)}`)
+
+    const startingDate = makeAnHTMLLi(`Starting date: ${event.target.startingDate.value}`)
 
     const title = makeAnHTMLLi(`Title: ${event.target.title.value}`)
 
     const salary = makeAnHTMLLi(`Salary: £ ${event.target.salary.value}`)
 
+
+
     const displayArea = document.querySelector("#results")
+    const displayManager = document.querySelector("#manager")
+    const displayChef = document.querySelector("#chef")
+    const displayFrontOfHouse = document.querySelector("#foh")
 
-    const arrayOfDetails = [name, surname, title, salary]
 
-    arrayOfDetails.forEach((el) => {
-      
-      return displayArea.appendChild(el)
-    })
+    const detailsNewStaff = [name, surname, startingDate, title, salary]
+
+
+
+
+      if (detailsNewStaff[3].textContent == "Title: Manager") {
+        detailsNewStaff.forEach((el) => {
+          return displayManager.appendChild(el)
+      })
+    }
+    else if (detailsNewStaff[3].textContent == "Title: Chef") {
+      detailsNewStaff.forEach((el) => {
+        return displayChef.appendChild(el)
+      })
+    }
+    else  detailsNewStaff.forEach((el) => {
+        return displayFrontOfHouse.appendChild(el)
+      })
+
+
+
 
     event.target.reset()
 
   }
 
   handleDeleteAll = function() {
-    const displayArea = document.querySelector("#results")
-    displayArea.innerHTML = " "
+    const displayManager = document.querySelector("#manager")
+    const displayChef = document.querySelector("#chef")
+    const displayFrontOfHouse = document.querySelector("#foh")
+    displayManager.innerHTML = " "
+    displayChef.innerHTML = " "
+    displayFrontOfHouse.innerHTML = " "
 
   }
